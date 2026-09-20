1062e0efc3f3fb80e3fe06ec823dbbbcb5f12f5e
Alexei <alexalekov7@gmail.com>
2026-09-20 16:27:11 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,16 @@
-# bot.py
 import telebot
 from config import BOT_TOKEN
 from database import Database
 from handlers import BotHandlers
 
+
 def main():
     bot = telebot.TeleBot(BOT_TOKEN)
-
     Database.create_tables()
-    print("✅Бд")
-
     handlers = BotHandlers(bot)
     handlers.register_handlers()
-    print("✅handlers")
-    
-    print("✅Бот запущен")
     bot.infinity_polling()
+
 
 if __name__ == "__main__":
     main()
